b7861ee7cc7cbe3a45d26cbb41d470cec2986959
chung <cflawaa01@gmail.com>
2023-10-28 12:48:31 +0100

    add ball and creating motion

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-document.querySelector("canvas").width = "800";
-document.querySelector("canvas").height = "600";
+const canvaswidth = 800;
+const canvasheight = 600;
+const start = Math.floor(Math.random()*4);
+document.querySelector("canvas").width = canvaswidth;
+document.querySelector("canvas").height = canvasheight;
 let canvasCtx = document.querySelector("canvas").getContext("2d");
 let userpaddle = {
     size : {
@@ -14,6 +17,21 @@ movingLeft : false,
 movingRight : false
 
 };
+const ball = {
+    size : 8,
+    position : {
+        x : 400 ,
+        y : 350
+    }
+}
+class Blocks{
+    constructor(x,y){
+        this.size = {width:80,height:30};
+        this.position = {x,y}
+    }
+
+}
+
 canvasCtx.fillStyle = "black";
 function renderUserpaddle(){
 canvasCtx.fillRect( userpaddle.position.x,userpaddle.position.y,userpaddle.size.width,userpaddle.size.height);
@@ -24,9 +42,73 @@ document.addEventListener("keydown", (e)=>
 }else if (e.code == "ArrowRight"){
     userpaddle.movingRight = true
 }
-
+})
+document.addEventListener("keyup", (e)=>
+{ if (e.code == "ArrowLeft"){
+    userpaddle.movingLeft = false
+}else if (e.code == "ArrowRight"){
+    userpaddle.movingRight = false
+}
 })
 function moveUserPaddle(){
+    if(userpaddle.movingLeft == true && userpaddle.position.x > 0){
+        userpaddle.position.x -= 15;
 
+    }else if(userpaddle.movingRight == true && userpaddle.position.x < canvaswidth - userpaddle.size.width){
+        userpaddle.position.x +=15;
+      
+    }
 
+}
+function updateCanvas(){
+    canvasCtx.clearRect(0,0,canvaswidth,canvasheight)
+    moveUserPaddle();
+    renderUserpaddle();
+    createBall();
+    
+    
+   
+
+}
+setInterval(() =>{
+    updateCanvas()
+},20)
+
+/* function createBlocks(){
+    for(i=1;i<7;i++){
+        const blockrow = new Blocks(10, i*40);
+        canvasCtx.fillRect(blockrow.position.x,blockrow.position.y,blockrow.size.width,blockrow.size.height);
+   /*  for (i=0;i<8;i++){
+        let newblock = new Blocks(i*100+10,50);
+        canvasCtx.fillRect(newblock.position.x,newblock.position.y,newblock.size.width,newblock.size.height)
+    } */
+
+function createBall(){
+    canvasCtx.save();
+    canvasCtx.beginPath();
+canvasCtx.arc(ball.position.x, ball.position.y, ball.size, 0, 2 * Math.PI);
+canvasCtx.fillStyle = "blue";
+canvasCtx.fill();
+canvasCtx.restore();
+
+}
+
+function moveBall(){
+    
+    if(start == 0){
+    ball.position.x +=3
+    ball.position.y +=3
+    }else if(start == 1){
+        ball.position.x -=3
+        ball.position.y +=3
+    
+    }else if(start == 2){
+        ball.position.x -=3
+        ball.position.y -=3
+    
+    }else {
+        ball.position.x +=3
+        ball.position.y -=3
+
+    }
 }
