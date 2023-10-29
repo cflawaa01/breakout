1617cf35414252aade02356d3eb6d6a328e3d426
chung <cflawaa01@gmail.com>
2023-10-29 12:18:29 +0000

    add wall collision

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const canvaswidth = 800;
 const canvasheight = 600;
-const start = Math.floor(Math.random()*4);
+let start = Math.floor(Math.random()*4);
 document.querySelector("canvas").width = canvaswidth;
 document.querySelector("canvas").height = canvasheight;
 let canvasCtx = document.querySelector("canvas").getContext("2d");
@@ -17,13 +17,26 @@ movingLeft : false,
 movingRight : false
 
 };
-const ball = {
+const ball= {
     size : 8,
     position : {
         x : 400 ,
         y : 350
+    },
+    speed : 3,
+    directionX : (x)=>{
+        ball.position.x += x*ball.speed
+    },
+    directionY : (x)=>{
+        ball.position.y += x*ball.speed
     }
-}
+
+    }
+   
+
+
+  
+
 class Blocks{
     constructor(x,y){
         this.size = {width:80,height:30};
@@ -65,6 +78,9 @@ function updateCanvas(){
     moveUserPaddle();
     renderUserpaddle();
     createBall();
+    moveBall();
+    
+    
     
     
    
@@ -96,19 +112,37 @@ canvasCtx.restore();
 function moveBall(){
     
     if(start == 0){
-    ball.position.x +=3
-    ball.position.y +=3
+   ball.directionX(-1);
+   ball.directionY(-1)
     }else if(start == 1){
-        ball.position.x -=3
-        ball.position.y +=3
+   ball.directionX(+1);
+   ball.directionY(-1)
     
     }else if(start == 2){
-        ball.position.x -=3
-        ball.position.y -=3
+   ball.directionX(-1);
+   ball.directionY(+1)
     
     }else {
-        ball.position.x +=3
-        ball.position.y -=3
+     ball.directionX(+1);
+     ball.directionY(+1)
 
     }
-}
+    if(ball.position.x<=0 && start ==0){
+        start = 1
+    }else if(ball.position.x<=0 && start ==2){
+        start = 3}
+    else if(ball.position.x>=canvaswidth && start ==1){
+            start = 0
+    }else if(ball.position.x>=canvaswidth && start ==3){
+        start = 2
+    }else if(ball.position.y<=0 && start ==0){
+        start = 2}
+    else if(ball.position.y<=0 && start ==1){
+            start = 3}
+    else if(ball.position.y >= canvasheight && start ==2){
+                start = 0}
+    else if(ball.position.y >= canvasheight && start ==3){
+                    start = 1}
+            
+    }
+
